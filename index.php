4268b17de55f5a82c589dfa3b38fde110ecd19e9
tamiratsadghy <gitman754@gmail.com>
2024-12-21 10:26:43 +0330

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 include("header.html");
 ?>
+<h1>
+ <img id="img4" src="https://vgdl.ir/wp-content/uploads/2024/11/animation-download-1.gif"style="width: 330px;height: 222px;" >
+ </h1>
     <p class="sb">صفحه اصلی </p>
     <h1>تعمیرات انواع موتور 125cc تا  250cc </h1>
 <h4>فروش لوازم تزینی موتور و🏍.</h4>
 <h1>تراش کاری و قعطه سازی</h1>
+
+
+
+<video src="cb.mp4" controls style="width: 555px;height: 666px;px;"></video>
 
 <?php
 include("footer.html");
